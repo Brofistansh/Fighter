f83725c6d24682442e4a3297410893626f79069e
Ansh Jaiswal <jaiansh2016@gmail.com>
2024-07-23 13:59:00 +0530

    player1move movement done

--- Assets/MyScripts/Player1move.cs
+++ Assets/MyScripts/Player1move.cs
@@ -5,23 +5,32 @@ using UnityEngine;
 public class Player1move : MonoBehaviour
 {
     private Animator Anim;
+    public float WalkSpeed =0.01f;
     // Start is called before the first frame update
     void Start()
     {
-        Anim = GetComponent<Animator>();
+        Anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Anim == null)
+    {
+        Debug.LogError("Animator component not found!");
+        return;
+    }
+
         //walking left and right 
         if(Input.GetAxis("Horizontal")>0)
         {
             Anim.SetBool("Forward",true);
+            transform.Translate(WalkSpeed,0,0);
         }
         if(Input.GetAxis("Horizontal")<0)
         {
             Anim.SetBool("Backward",true);
+            transform.Translate(-WalkSpeed,0,0);
         }
         if(Input.GetAxis("Horizontal")==0)
         {
